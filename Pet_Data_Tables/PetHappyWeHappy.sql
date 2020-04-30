125b61adf36749c175752160c3bf1ce1df0497b7
Huiling Chen <huiling_chen@berkeley.edu>
2020-04-29 18:22:44 -0700

    Update foreign key part

--- Pet_Data_Tables/PetHappyWeHappy.sql
+++ Pet_Data_Tables/PetHappyWeHappy.sql
@@ -129,7 +129,7 @@ CREATE TABLE Grooming_Services(
          GService_ID INT(2) NOT NULL AUTO_INCREMENT,
          Service_name VARCHAR(40) NOT NULL,
          PRIMARY KEY (GService_ID)
-     );
+     )ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Grooming_Services.csv'
 INTO TABLE Grooming_Services
@@ -138,16 +138,16 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-DROP TABLE  IF EXISTS 'Groomer';
+DROP TABLE  IF EXISTS `Groomer`;
 CREATE TABLE Groomer (
     Groomer_ID int(3) NOT NULL AUTO_INCREMENT,
     Groomer_name varchar(40) NOT NULL,
     City VARCHAR(20) NOT NULL ,
     State VARCHAR(2) NOT NULL,
     PRIMARY KEY (Groomer_ID)
-);
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Groomer.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Groomer.csv'
 INTO TABLE Groomer
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -164,12 +164,12 @@ CREATE TABLE Grooming_Record (
     Price int(3),
     Service_time int(3),
     PRIMARY KEY (GRecord_ID),
-    FOREIGN KEY (GService_ID) REFERENCES Grooming_Services(GService_ID),
-    FOREIGN KEY (Groomer_ID) REFERENCES Groomer(Groomer_ID)
-);
+    FOREIGN KEY (GService_ID) REFERENCES Grooming_Services(GService_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Groomer_ID) REFERENCES Groomer(Groomer_ID) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Grooming_Record.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Grooming_Record.csv'
 INTO TABLE Grooming_Record
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -182,10 +182,10 @@ CREATE TABLE Pet_Training_Type (
     Training_ID INT(2) NOT NULL AUTO_INCREMENT,
     Training_name VARCHAR(40) NOT NULL,
     PRIMARY KEY (Training_ID)
-;)
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Pet_Training_Type.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Pet_Training_Type.csv'
 INTO TABLE Pet_Training_Type
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -199,9 +199,9 @@ CREATE TABLE Pet_Training_Agent (
     City VARCHAR(20) NOT NULL ,
     State VARCHAR(2) NOT NULL,
     PRIMARY KEY (Agent_ID)
-);
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Pet_Training_Agent.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Pet_Training_Agent.csv'
 INTO TABLE Pet_Training_Agent
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -217,13 +217,13 @@ CREATE TABLE Pet_Training_Record (
     Training_time INT(2),
     Training_cost INT(4),
     PRIMARY KEY (TRecord_ID),
-#     FOREIGN KEY (Pet_ID) references Pet(Pet_ID)
-    FOREIGN KEY (Training_ID) REFERENCES Pet_Training_Type(Training_ID),
-    FOREIGN KEY (Agent_ID) REFERENCES Pet_Training_Agent(Agent_ID)
-);
+#     FOREIGN KEY (Pet_ID) references Pet(Pet_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Training_ID) REFERENCES Pet_Training_Type(Training_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Agent_ID) REFERENCES Pet_Training_Agent(Agent_ID) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Pet_Training_Records.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Pet_Training_Records.csv'
 INTO TABLE Pet_Training_Record
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
@@ -239,18 +239,18 @@ CREATE TABLE Friend_Match_List
     Friend_ID INT(8) NOT NULL,
     Like_this_friend BOOLEAN NOT NULL,
     PRIMARY KEY (Match_ID)
-#     FOREIGN KEY Pet_ID references Pet(Pet_ID),
-#     FOREIGN KEY Friend_ID references Pet(Pet_ID),
-);
+#     FOREIGN KEY Pet_ID references Pet(Pet_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+#     FOREIGN KEY Friend_ID references Pet(Pet_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+)ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 
-LOAD DATA LOCAL INFILE '/Users/CHELSEY/Desktop/SPRING 2020/INFO 257/PETSOCIALPLATFORM/data/Friend_Matching_List.csv'
+LOAD DATA LOCAL INFILE '/Users/CHELSEY/Documents/Github/info257-gitlcone/Pet-Social-Platform-App/Pet_Data_Tables/Friend_Matching_List.csv'
 INTO TABLE Friend_Match_List
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
-
+#__________________________
 DROP TABLE IF EXISTS Breeding_Record;
 CREATE TABLE Breeding_Record (
     BRecord_ID INT(7) NOT NULL AUTO_INCREMENT,
